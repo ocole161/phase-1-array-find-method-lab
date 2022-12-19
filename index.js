7bfd0fd3ab683a995b8f9efbba3d289903abb53b
Owen Cole <ocole161@gmail.com>
2022-12-19 15:06:45 -0700

    commit tree dbae6ac7886f77d1752408686548ebed6219fe58

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // code your solution here
+
+function isWin(record) {
+    return (record.result === "W")
+}
+
+function superbowlWin(record) {
+    let result
+    if (record.find(isWin)) {
+        result = record.find(isWin).year
+    }
+    else {
+        result = record.find(isWin)
+    }
+    return result
+}
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+]
+
+// console.log(isWin(record))
+
+// console.log(record.find(isWin))
+
+// console.log(record.find(isWin).result)
+
+// console.log(record.find(isWin).year) 
+
+console.log(superbowlWin(record))
